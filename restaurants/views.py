fa3b495294eee1da888464548c939d260b62fc54
Mariam <Mariamhh98@hotmail.com>
2019-08-20 12:18:53 +0300

    Amnah and Mariam

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def step7(request):
+    context = {
+            "msg": "Hello World!",
+            }
+    return render(request, "html_file.html", context)
